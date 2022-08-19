a510c0dd84696565f573616e07e096d56e44ffac
Wasim <wk356414@gmail.comm>
2022-08-19 18:26:12 +0530

    icon issue solved

--- src/Pages/Common/Header.js
+++ src/Pages/Common/Header.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 // import fontawesome from '@fortawesome/fontawesome'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faCheckSquare, faCoffee } from '@fortawesome/fontawesome-free-solid'
+import {  faFacebookF, faTwitter, faYoutube, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 import pic from "../../assets/img/logo.png";
 
 export default class Header extends Component {
@@ -30,10 +32,10 @@ export default class Header extends Component {
                     <li><Link to={"/contact"}><i className="fas fa-user"></i>Contact Us</Link></li>
                     <li><a href="tel:+917988174873"><FontAwesomeIcon icon="fa-solid fa-phone" />Call now</a></li>
                     <div className="icons">
-                      <a href={() => false}><i className="fab fa-facebook-f"></i></a>
-                      <a href={() => false}><i className="fab fa-twitter"></i></a>
-                      <a href={() => false}><i className="fab fa-github"></i></a>
-                      <a href={() => false}><i className="fab fa-youtube"></i></a>
+                      <a href={() => false}><FontAwesomeIcon icon={faFacebookF} /></a>
+                      <a href={() => false}><FontAwesomeIcon icon={faTwitter} /></a>
+                      <a href={() => false}><FontAwesomeIcon icon={faYoutube} /></a>
+                      <a href={() => false}><FontAwesomeIcon icon={faInstagram} /></a>
                     </div>
                   </ul>
                 </nav>
